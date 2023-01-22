ea2b657f518bd876da7f27d1d523a27a5fec106b
menem <Mohamedabdelmonem1>
2023-01-22 16:28:20 +0200

    screens

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+
+import 'package:fff/auth/signin.dart';
+import 'package:fff/auth/splashscreen.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:  SplashScreen(),
+    );
+  }
+}
